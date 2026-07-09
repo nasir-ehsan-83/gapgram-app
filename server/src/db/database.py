a49a0a168c9fad90d20e03872be7bf9def576c0f
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-09 13:18:51 +0430

    refactor(db): update import directories and transform 'config/database.py' to 'db/database.py'

---
+++ server/src/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import  create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
+from src.config import settings
+
+SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo = True)
+
+AsyncSesstionLocal = async_sessionmaker(
+    bind = engine,
+    class_ = AsyncSession,
+    expire_on_commit = False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSesstionLocal() as session:
+        yield session
