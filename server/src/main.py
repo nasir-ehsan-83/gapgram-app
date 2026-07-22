1962dd5261b9822a55736753101d651a72d9b18e
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-22 22:15:11 +0430

    feat: implement FastAPI lifespan for database initialization

--- server/src/main.py
+++ server/src/main.py
@@ -1,20 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.common.errors.handlers import init_error_handlers
-from server.src.db import database
-from server.src.modules.auth import routes as auth_routes
-from server.src.modules.users import routes as users_routes
-from server.src.modules.posts import routes as posts_routes
-from server.src.modules.votes import routes as votes_routes
+from src.db import database
 
-app = FastAPI()
+from src.modules.auth import routes as auth_routes
+from src.modules.users import routes as users_routes
+from src.modules.posts import routes as posts_routes
+from src.modules.votes import routes as votes_routes
 
-@app.on_event("startup")
-async def init_tables():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     async with database.engine.begin() as conn:
         await conn.run_sync(database.Base.metadata.create_all)
+    yield
+    await database.engine.dispose()
 
-# Activate the robust error handling system globally
+app = FastAPI(lifespan = lifespan)
+
+
 init_error_handlers(app)
 
 app.include_router(auth_routes.router)
